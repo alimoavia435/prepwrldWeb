ef385a1d0cc659e60b992c4b341ce812f1f1408e
alimoavia435 <maharalimoavia435@gmail.com>
2025-04-03 10:47:28 +0500

    update

--- src/services/client.js
+++ src/services/client.js
@@ -3,4 +3,5 @@
 // export const API_URL = "http://192.241.137.144:5000";
 // export const API_URL = "https://evox.blockmoblabs.com";
 
-export const API_URL = "https://multiple-tutors.vercel.app";
+// export const API_URL = "https://multiple-tutors.vercel.app";
+export const API_URL = "https://prepwrldmanagement.site";
